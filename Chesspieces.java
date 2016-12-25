358e787c73ad05a5a6c47189f7372418e71e7113
Laura <laura.gooon@gmail.com>
2016-12-24 19:16:25 -0500

    locationtoInt fxn compiles

--- Chesspieces.java
+++ Chesspieces.java
@@ -1,36 +1,30 @@
 public abstract class Chesspieces{
 
-    //example: a3 --> substring/parseInt into 'a' and 3 --> change into 13 
-    public int currentLocation;
+  //example: a3 --> substring/parseInt into 'a' and 3 --> change into 13
+  public int currentLocation;
 
-    //moves piece @ current to newlocation: use substring to analyze locations
-    //maybe use a switch case to figure out what'a-e' represent in terms of
-    //2d array
-    //the first thing move is going to do is change newlocation to int
-    abstract void move(String newlocation);
+  //moves piece @ current to newlocation: use substring to analyze locations
+  //maybe use a switch case to figure out what'a-e' represent in terms of
+  //2d array
+  //the first thing move is going to do is change newlocation to int
+  abstract void move(String newlocation);
 
-    abstract boolean isValid(String newlocation);
+  abstract boolean isValid(String newlocation);
 
-    public static int locationtoInt(String location){
-	this.currentLocation = 0;
-	switch(location.substring(0,1)){
-	case "a": return 10 + Integer.parseInt(location.substring(1));
-	    break;
-	case "b": return 20 + Integer.parseInt(location.substring(1));
-	    break;
-	case "c": return 30 + Integer.parseInt(location.substring(1));
-	    break;
-	case "d": return 40 + Integer.parseInt(location.substring(1));
-	    break;
-	case "e": return 50 + Integer.parseInt(location.substring(1));
-	    break;
-	case "f": return 60 + Integer.parseInt(location.substring(1));
-	    break;
-	case "g": return 70 + Integer.parseInt(location.substring(1));
-	    break;
-	case "h": return 80 + Integer.parseInt(location.substring(1));
-	    break;
-	    
-	}
+
+  public static int locationtoInt(String location){
+    int ret = 0;
+    switch(location.substring(0,1)){
+      case "a": ret =  10 + Integer.parseInt(location.substring(1));
+      case "b": ret =  20 + Integer.parseInt(location.substring(1));
+      case "c": ret =  30 + Integer.parseInt(location.substring(1));
+      case "d": ret =  40 + Integer.parseInt(location.substring(1));
+      case "e": ret =  50 + Integer.parseInt(location.substring(1));
+      case "f": ret =  60 + Integer.parseInt(location.substring(1));
+      case "g": ret =  70 + Integer.parseInt(location.substring(1));
+      case "h": ret =  80 + Integer.parseInt(location.substring(1));
+      default: break;
     }
+    return ret;
+  }
 }

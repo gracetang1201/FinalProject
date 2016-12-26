5cbf95d61e3a9be8f8c56432d03e1b47f4610eff
gracetang1201 <gracetang1201@gmail.com>
2016-12-26 13:31:37 -0500

    JK THE CONSTRUCTOR WORKS NOW IT'LL MAKE EVERYTHING ON BOTH SIDES LITTY

--- Chessboard.java
+++ Chessboard.java
@@ -10,41 +10,38 @@ public class Chessboard{
 	char pieceAt = ' ';
 	for (int r = 0; r < 8; r++){
 	    for (int c = 0; c < 8; c++){
-		if (r == 7 || r == 0){
-		    switch(c){
-		    case 0: pieceAt = 'R';
-			break;
-		    case 1: pieceAt = 'N';
-			break;
-		    case 2: pieceAt = 'B';
-			break;
-		    case 3: pieceAt = 'Q';
-			break;
-		    case 4: pieceAt = 'K';
-			break;
-		    case 5: pieceAt = 'B';
-			break;
-		    case 6: pieceAt = 'N';
-			break;
-		    case 7: pieceAt = 'R';
-			break;
-		           
-		    }
-		}
+	 
 		if (r == 6 || r == 1){
-		    pieceAt = 'P';
-		}
-		cb[r][c] = pieceAt;
-	    }
+		    cb[r][c] = 'P';
+		
+		}else{
+		    if (r == 7 || r == 0){
+		
+		    switch(c){
+		    case 0: cb[r][c] = 'R';
+			break;
+		    case 1: cb[r][c] = 'N';
+			break;
+		    case 2:cb[r][c]  = 'B';
+			break;
+		    case 3:cb[r][c] = 'Q';
+			break;
+		    case 4: cb[r][c] = 'K';
+			break;
+		    case 5:cb[r][c] = 'B';
+			break;
+		    case 6:cb[r][c] = 'N';
+			break;
+		    case 7: cb[r][c] = 'R';
+			break;
+		    }
+		    
+		}else{
+			cb[r][c] = '.';
+	    }}
 	    
-	}
-	
-	for(int r = 2; r < 8; r++){
-	    for(int c = 0; c < 8; c++){
-		cb[r][c] = '.';
-	    }
-	}
-    }
+	    }}}
+
     public String toString(){
 	String ans = "";
 	for (int r = 0; r < 8; r++){

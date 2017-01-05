354a81dee2ea61250a72c77801c0cf5ef596f0f7
Laura Goon <laura.gooon@gmail.com>
2017-01-05 12:29:04 -0500

    check if xor works

--- Pawn.java
+++ Pawn.java
@@ -2,25 +2,31 @@ public class Pawn extends Chesspieces{
     private char player;
     private int currentLocation;
     public Pawn(int x, int y, char p){
-currentLocation = 10*x + y;
-player = p;
+	currentLocation = 10*x + y;
+	player = p;
     }
     public String toString(Pawn p){
-return "P";
+	return "P";
     }
-    Chessboard chessb = new Chessboard();
+    
     public void move(String newlocation){
-if(isValid newlocation){
-   currentLocation = locationtoInt(newlocation);
-}
+	if(isValid newlocation){
+	    currentLocation = locationtoInt(newlocation);
+	}
     }
     public boolean isValid(String newlocation){
-int newloc = locationtoInt(newlocation);
-if (currentLocation + 1 == newloc){
-   return true;
-}
-if (currentLocation + 11 == newloc){
-   return true;
-}
+	int newloc = locationtoInt(newlocation);
+	if (currentLocation + 1 == newloc){
+	    return true;
+	}
+	if (currentLocation + 2 == newloc){
+	    return true;
+	}
+	if (currentLocation + 11 == newloc){ // not valid if nothing in newloc
+	    return true;
+	}
+	if (currentLocation + 9 == newloc){ // not valid if nothing in new loc
+	    return true;
+	}
     }
 }

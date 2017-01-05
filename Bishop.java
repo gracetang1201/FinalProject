a24ca6b34aca96a003353770054cac631bee11e9
gracetang1201 <gracetang1201@gmail.com>
2017-01-04 19:37:30 -0500

    added onto bishop

--- Bishop.java
+++ Bishop.java
@@ -2,14 +2,22 @@ public class Bishop extends Chesspieces{
     private char player;
     private int currentLocation;
     public Bishop(int x, int y, char p){
-currentLocation = 10*x + y;
-player = p;
+	currentLocation = 10*x + y;
+	player = p;
     }
-    public String toString(Pawn p){
-return "B";
+    public String toString(Bishop b){
+	return "B";
     }
     public void move(String newlocation){
-if(isValid (newlocation)){
-
-}
-}
+	if(isValid (newlocation)){
+	    currentLocation = locationtoInt(newlocation);
+	}
+    }
+    public boolean isValid(String newlocation){
+	if ((locationtoInt(newlocation) - currentLocation)%11 == 0){
+	    return true;
+	}
+	if ((locationtoInt(newlocation) - currentLocation)%9 == 0){
+	    return true;
+	}
+    }

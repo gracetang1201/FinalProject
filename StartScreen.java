22fd46d0e01eac8a0ac1af77c414962497da97ce
Laura Goon <laura.gooon@gmail.com>
2017-01-07 16:31:20 -0500

    flourished start screen

--- StartScreen.java
+++ StartScreen.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
 public class StartScreen{
+
+  public static final String ANSI_RESET = "\u001B[0m";
+  public static final String ANSI_BLACK = "\u001B[30m";
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_GREEN = "\u001B[32m";
+  public static final String ANSI_YELLOW = "\u001B[33m";
+  public static final String ANSI_BLUE = "\u001B[34m";
+  public static final String ANSI_PURPLE = "\u001B[35m";
+  public static final String ANSI_CYAN = "\u001B[36m";
+  public static final String ANSI_WHITE = "\u001B[37m";
+
   private static String chess1 = "      ___           ___           ___           ___           ___     ";
   private static String chess2 = "     /  /\\         /__/\\         /  /\\         /  /\\         /  /\\    ";
   private static String chess3 = "    /  /:/         \\  \\:\\       /  /:/_       /  /:/_       /  /:/_   ";
@@ -17,7 +28,13 @@ public class StartScreen{
     System.out.println(chess1+newLine+chess2+newLine+chess3+newLine+chess4+newLine+chess5+newLine+chess6+newLine+chess7+newLine+chess8+newLine+chess9+newLine+chess10+newLine+chess11+newLine);
     try{
       Thread.sleep(1000);
-      System.out.println("                         -- Rook No Further --");
+      System.out.println(ANSI_RED + "                         -- Rook No Further --" + ANSI_RESET);
+      // System.out.println(ANSI_GREEN + "                         -- Rook No Further --" + ANSI_RESET);
+      // System.out.println(ANSI_YELLOW + "                         -- Rook No Further --" + ANSI_RESET);
+      // System.out.println(ANSI_BLUE + "                         -- Rook No Further --" + ANSI_RESET);
+      // System.out.println(ANSI_PURPLE + "                         -- Rook No Further --" + ANSI_RESET);
+      // System.out.println(ANSI_CYAN + "                         -- Rook No Further --" + ANSI_RESET);
+      // System.out.println(ANSI_WHITE + "                         -- Rook No Further --" + ANSI_RESET);
     } catch(InterruptedException e){
       System.out.println("INTERRUPTED");
     }
@@ -31,9 +48,9 @@ public class StartScreen{
       Thread.sleep(1000);
       System.out.println();
       System.out.println();
-      System.out.println("          type   :instructions          for instructions");
-      System.out.println("          type   :start                 to start game");
-      System.out.println("          type   :quit                  to quit at anytime");
+      System.out.println(ANSI_CYAN +"          type   :"+ ANSI_WHITE + "instructions          "+ ANSI_GREEN +"for instructions");
+      System.out.println(ANSI_CYAN +"          type   :"+ ANSI_WHITE +"start                 "+ ANSI_GREEN +"to start game");
+      System.out.println(ANSI_CYAN +"          type   :"+ ANSI_WHITE +"quit                  "+ ANSI_GREEN +"to quit at anytime"+ ANSI_RESET);
     } catch(InterruptedException e){
       System.out.println("INTERRUPTED");
     }

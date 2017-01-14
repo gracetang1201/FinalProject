7147efb61662c28e98d02c6bf30967556fa081f3
Laura Goon <laura.gooon@gmail.com>
2017-01-14 12:36:32 -0500

    javadoc comments

--- Bishop.java
+++ Bishop.java
@@ -1,18 +1,35 @@
 public class Bishop extends Chesspieces{
   private char player;
   private String currentLocation;
+
+/**
+ * Class constructor specifying location and whose user it belongs to.
+ */
   public Bishop(int x, int y, char p){
     currentLocation = Integer.toString(y) + Integer.toString(x);
     player = p;
   }
+
+/**
+ * Returns letter representation
+ * @return the letter "B"
+ */
   public String toString(){
     return "B";
   }
 
+/**
+ * Gets the private player variable
+ * @return which player this chesspiece belongs to
+ */
   public String getPlayer(){
     return Character.toString(player);
   }
 
+/**
+ * Works in conjunction with isValid() to determine if move is valid
+ * @return if move is valid
+ */
   public boolean move(String newlocation){
     if(isValid (newlocation)){
       currentLocation = locationtoInt(newlocation);
@@ -21,6 +38,11 @@ public class Bishop extends Chesspieces{
 	return false;
     }
   }
+
+/**
+ * Holds the chess rules for the Bishop piece
+ * @return if move would ve valid
+ */
   public boolean isValid(String newlocation){
       int newl = Integer.parseInt(super.locationtoInt(newlocation));
       int curl = Integer.parseInt(currentLocation);
--- Blank.java
+++ Blank.java
@@ -4,21 +4,36 @@ public class Blank extends Chesspieces{
   private String currentLocation;
     private char player;
 
+  /**
+   * Class constructor specifying location
+   */
     public Blank(int x, int y){
     currentLocation = Integer.toString(y) + Integer.toString(x);
     player = 'n';
   }
 
+/**
+ * Gets private player variable
+ * @return which user this chesspiece belongs to
+ */
   public String getPlayer(){
     return Character.toString(player);
   }
 
+/**
+ * @return false because blanks cannot be moved
+ */
   public boolean move(String newlocation){
     return false;
   }
   public boolean isValid(String newlocation){
     throw new UnsupportedOperationException();
   }
+
+  /**
+   * Gets string representation of piece
+   * @return the string "."
+   */
   public String toString(){
     return ".";
   }
